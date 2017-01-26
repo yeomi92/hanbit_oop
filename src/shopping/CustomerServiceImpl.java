6bdf799d920a47dec1b37d76f87adfd60536d18b
yheisun <yheisun@gmail.com>
2017-01-26 11:03:49 +0900

    shopping

--- src/shopping/CustomerServiceImpl.java
+++ src/shopping/CustomerServiceImpl.java
@@ -1,30 +1,32 @@
 package shopping;
 
 public class CustomerServiceImpl implements CustomerService{
-	Customer c;
+	Customer customer;
+	Product[] cart;
 	int i;
 	public CustomerServiceImpl() {
-		c= new Customer();
+		customer= new Customer();
+		cart = new Product[4];
 		i=0;
 	}
 	//물건 구매 여부 확인, 부족하면 잔액부족
 	@Override
 	public String buy(Product p) {
-		int money = c.getMoney();
-		String result="잔액부족\n잔액: "+c.getMoney();
+		int money = customer.getMoney();
+		String result="잔액부족\n잔액: "+customer.getMoney();
 		if(money>p.getPrice()){
 			add(p);
-			result=p.getName()+"를 카트에 추가\n잔액: "+c.getMoney();
+			result=p.getName()+"를 카트에 추가\n잔액: "+customer.getMoney();
 		}
 		return result;
 	}
 	//카트에 추가시키는 로직
 	@Override
 	public void add(Product p) {
-		int money=c.getMoney();
+		int money=customer.getMoney();
 		money-=p.getPrice();
-		c.setMoney(money);
-		c.getCart()[i]=p;
+		customer.setMoney(money);
+		cart[i]=p;
 		i++;
 	}
 	//쇼핑을 마치고 영수증 출력 (toString과 잔액)
@@ -32,9 +34,9 @@ public class CustomerServiceImpl implements CustomerService{
 	public String getBill() {
 		String bill="";
 		for(int k=0;k<i;k++){
-			bill+=""+c.getCart()[k].getName()+"..."+c.getCart()[k].getPrice()+"\n";
+			bill+=""+cart[k].getName()+"..."+cart[k].getPrice()+"\n";
 		}
-		bill+="잔액: "+c.getMoney();
+		bill+="잔액: "+customer.getMoney();
 		return bill;
 	}
 }
--- src/shopping/Himart.java
+++ src/shopping/Himart.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 public class Himart {
 	public static void main(String[] args) {
 		CustomerService service = new CustomerServiceImpl();
-		Customer c = new Customer();
 		while(true){
 			switch(JOptionPane.showInputDialog("1.청소기 2.컴퓨터 3.스마트폰 4.TV 5.영수증 0.종료")){
 				case"0":return;
